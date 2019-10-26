2066310ffe5558cc518cb7f890c3d51e10ae4521
Jack Staples <jackstaplesjcrs@gmail.com>
2019-10-26 16:35:18 -0700

    add color scalebanding

--- src/App.js
+++ src/App.js
@@ -11,20 +11,14 @@ const App = () => {
 
   const [teams, setTeams] = useState([]);
   const [selected, setSelected] = useState([]);
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    fetchNhlTeams();
-    const svg = d3.select(svgRef.current);
-    //makeBinnedBarchart(svg, data);
-  }, []);
-
-  const fetchNhlTeams = async () => {
-    const response = await fetch("https://statsapi.web.nhl.com/api/v1/teams");
-    const nhlData = await response.json();
-    setTeams(nhlData.teams);
-  };
-
-  const fetchNHLTeamData = async () => {};
+    fetchNhlTeams().then(() => {
+      const svg = d3.select(svgRef.current);
+      makeBinnedBarchart(svg, players);
+    });
+  }, [players]);
 
   const handleSelect = (event, data) => {
     const { id } = event.target;
@@ -35,31 +29,84 @@ const App = () => {
     } else {
       newSelected = update(selected, { $push: [id] });
     }
-    console.log(selected);
     setSelected(newSelected);
+    fetchNHLTeamData(newSelected);
+  };
+
+  const fetchNhlTeams = async () => {
+    const response = await fetch("https://statsapi.web.nhl.com/api/v1/teams");
+    const nhlData = await response.json();
+    setTeams(nhlData.teams);
+  };
+
+  const fetchNHLTeamData = async selected => {
+    const teamsResponse = await fetch(
+      `https://statsapi.web.nhl.com/api/v1/teams?teamId=${selected}&expand=team.roster`
+    );
+    const teamData = await teamsResponse.json();
+    fetchNHLPlayerData(teamData);
+  };
+
+  const fetchNHLPlayerData = async teamData => {
+    const players = [];
+
+    teamData.teams.forEach(async team => {
+      team.roster.roster.forEach(async player => {
+        const playerResponse = await fetch(
+          `https://statsapi.web.nhl.com${player.person.link}`
+        );
+        const playerData = await playerResponse.json();
+        players.push(playerData);
+      });
+    });
+
+    setPlayers(players);
   };
 
   return (
     <React.Fragment>
-      <h1>{selected}</h1>
-      {teams.map(team => {
-        return (
-          <Checkbox
-            data={team}
-            handleSelect={handleSelect}
-            id={team.id}
-            key={team.id}
-          />
-        );
-      })}
+      <div>
+        {teams.map(team => {
+          return (
+            <Checkbox
+              data={team}
+              handleSelect={handleSelect}
+              id={team.id}
+              key={team.id}
+            />
+          );
+        })}
+      </div>
       <svg ref={svgRef} />
     </React.Fragment>
   );
 };
-/*
-const makeBinnedBarchart = (svg, data) => {
+
+const makeBinnedBarchart = (svg, players) => {
+  d3.select("g").remove();
+
+  const data = [
+    { month: "January", count: 0 },
+    { month: "February", count: 0 },
+    { month: "March", count: 0 },
+    { month: "April", count: 0 },
+    { month: "May", count: 0 },
+    { month: "June", count: 0 },
+    { month: "July", count: 0 },
+    { month: "August", count: 0 },
+    { month: "September", count: 0 },
+    { month: "October", count: 0 },
+    { month: "November", count: 0 },
+    { month: "December", count: 0 }
+  ];
+
+  players.forEach(player => {
+    let date = new Date(player.people[0].birthDate);
+    data[date.getMonth()].count += 1;
+  });
+
   // set the margins of the graph
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 },
+  const margin = { top: 20, right: 40, bottom: 30, left: 60 },
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -70,9 +117,14 @@ const makeBinnedBarchart = (svg, data) => {
     .domain([
       0,
       d3.max(data, d => {
-        return d.number;
+        return d.count;
       })
     ]);
+
+  const colorScale = d3.scaleOrdinal(d3.schemeSet3);
+
+  console.log(colorScale);
+
   const y = d3
     .scaleBand()
     .range([0, height])
@@ -90,13 +142,13 @@ const makeBinnedBarchart = (svg, data) => {
   // Scale the range of the data in the domains
   x.domain([
     0,
-    d3.max(data, function(d) {
-      return d.number;
+    d3.max(data, d => {
+      return d.count;
     })
   ]);
   y.domain(
-    data.map(function(d) {
-      return d.name;
+    data.map(d => {
+      return d.month;
     })
   );
 
@@ -105,18 +157,22 @@ const makeBinnedBarchart = (svg, data) => {
     .data(data)
     .enter()
     .append("rect")
-    .filter(d => {
-      return d.selected;
-    })
     .attr("class", "bar")
+    .attr("fill", d => {
+      console.log(y(d.month));
+      const color = colorScale(y(d.month));
+      console.log(color);
+      return color;
+    })
     .attr("x", 0)
     .attr("width", d => {
-      return y(d.name);
+      return d.count > 0 ? x(d.count) : 0;
     })
     .attr("y", d => {
-      return y(d.name);
+      return y(d.month);
     })
-    .attr("height", y.bandwidth());
+    .attr("height", y.bandwidth())
+    .on();
 
   // add the x Axis
   g.append("g")
@@ -126,5 +182,5 @@ const makeBinnedBarchart = (svg, data) => {
   // add the y Axis
   g.append("g").call(d3.axisLeft(y));
 };
-*/
+
 export default App;
